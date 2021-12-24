3d417a43d97272ad7ec92d93b9495465d46b5cc1
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-24 13:21:11 +0200

    Adds done task# 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[ data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+  if (intervalId) {
+    return;
+  }
+
+  intervalId = setInterval(() => {
+    const randomBodyColor = getRandomHexColor();
+    refs.body.style.backgroundColor = `${randomBodyColor}`;
+  }, 200);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  intervalId = null;
+}
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
