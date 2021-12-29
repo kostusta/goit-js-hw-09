3d0843da09e00d1e47366aa69b4722e60179d81a
Stanislav Kostiuchenko <kostiusta@gmail.com>
2021-12-29 17:49:44 +0200

    Adds fixes for task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -19,11 +19,33 @@ function onStartBtnClick() {
     const randomBodyColor = getRandomHexColor();
     refs.body.style.backgroundColor = `${randomBodyColor}`;
   }, 1000);
+
+  disableStartBtn();
+  anableStopBtn();
 }
 
 function onStopBtnClick() {
   clearInterval(intervalId);
   intervalId = null;
+
+  disableStopBtn();
+  anableStartBtn();
+}
+
+function disableStartBtn() {
+  refs.startBtn.setAttribute('disabled', 'true');
+}
+
+function anableStartBtn() {
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function disableStopBtn() {
+  refs.stopBtn.setAttribute('disabled', 'true');
+}
+
+function anableStopBtn() {
+  refs.stopBtn.removeAttribute('disabled');
 }
 
 refs.startBtn.addEventListener('click', onStartBtnClick);
